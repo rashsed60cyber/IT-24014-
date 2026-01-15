a0435f55a60eb8132ff290060886fb8846da3805
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 11:30:02 +0600

    Create Ch2Ex03.java

---
+++ Ch2Ex03.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ch2Ex03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a value for feet: ");
+        double feet = input.nextDouble();
+        double meters = feet * 0.305;
+        System.out.println(feet + " feet is " + meters + " meters");
+    }
+}
