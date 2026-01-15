8a1dedb8dce01bff72438e4819249198ebca78ce
rashsed60cyber <rashsed60@gmail.com>
2026-01-15 11:30:38 +0600

    Create Ch2Ex04.java

---
+++ Ch2Ex04.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ch2Ex04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number in pounds: ");
+        double pounds = input.nextDouble();
+        double kilograms = pounds * 0.454;
+        System.out.println(pounds + " pounds is " + kilograms + " kilograms");
+    }
+}
